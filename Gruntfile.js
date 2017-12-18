7589cd98bdf4f4f967d682af335e76ad32f08581
Arthur Hulsman <a.hulsman@orangegames.com>
2017-12-18 15:16:49 +0100

    VGD-551 - Added google cloud storage deploy task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,11 @@
-module.exports = function(grunt) {
+function atob(str) {
+    if (str) {
+        return new Buffer(str, 'base64').toString('binary');
+    }
+    return null;
+}
 
+module.exports = function(grunt) {
     const startTS = Date.now();
 
     grunt.initConfig({
@@ -165,6 +171,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-exec');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-google-cloud');
     grunt.loadNpmTasks('grunt-browser-sync');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browserify');
@@ -190,7 +197,6 @@ module.exports = function(grunt) {
             }
             console.log('Duration: ' + hh + ':' + mm + ':' + ss);
         });
-
     grunt.registerTask('sourcemaps', 'Build with sourcemaps', function() {
         grunt.config.set('uglify.options.sourceMap', true);
         grunt.config.set('uglify.options.sourceMapIncludeSources', true);
@@ -221,4 +227,76 @@ module.exports = function(grunt) {
             'duration'];
         grunt.task.run(tasksArray);
     });
+    grunt.registerTask('deploy',
+        'Upload the build files.',
+        function() {
+            const project = grunt.option('project'), // vooxe-gamedistribution
+                bucket = grunt.option('bucket'), // gd-sdk-html5
+                folderIn = grunt.option('in'), //
+                folderOut = grunt.option('out'); //
+
+            // The key is saved as a system parameter within Team City.
+            // The service account key of our google cloud account for uploading to
+            // storage is stringified and then encoded as base64 using btoa()
+            console.log(grunt.option('key'));
+            let keyObj = grunt.option('key');
+            let key = JSON.parse(atob(keyObj));
+            console.log(key);
+
+            if (project === undefined) {
+                grunt.fail.warn('Cannot upload without a project name');
+            }
+
+            if (bucket === undefined) {
+                grunt.fail.warn('OW DEAR GOD THEY ARE STEALING MAH BUCKET!');
+            }
+
+            if (key === undefined || key === null) {
+                grunt.fail.warn('Cannot upload without an auth key');
+            } else {
+                console.log('Key loaded...');
+            }
+
+            grunt.config.merge({
+                gcs: {
+                    options: {
+                        credentials: key,
+                        project: project,
+                        bucket: bucket,
+                        gzip: true,
+                        metadata: {
+                            'surrogate-key': 'gcs',
+                        },
+                    },
+                    dist: {
+                        cwd: './lib/',
+                        src: ['**/*'],
+                        dest: '',
+                    },
+                },
+            });
+
+            console.log('Project: ' + project);
+            console.log('Bucket: ' + bucket);
+
+            if (folderIn === undefined && folderOut === undefined) {
+                console.log('Deploying: ./lib/ to gs://' + bucket + '/');
+            } else {
+                if (folderIn !== undefined) {
+                    if (folderOut === undefined) {
+                        grunt.fail.warn(
+                            'No use in specifying "in" without "out"');
+                    }
+                    console.log('Deploying: ../' + folderIn + ' to gs://' +
+                        bucket + '/' + folderOut);
+                    grunt.config.set('gcs.dist', {
+                        cwd: '../' + folderIn, src: ['**/*'], dest: folderOut,
+                    });
+                } else if (folderOut !== undefined) {
+                    grunt.fail.warn('No use in specifying "out" without "in"');
+                }
+            }
+
+            grunt.task.run('gcs');
+        });
 };
