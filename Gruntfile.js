0037e7ff3cb600867045552329aa654f38926bd2
Arthur Hulsman <a.hulsman@orangegames.com>
2017-12-19 09:52:44 +0100

    Updated babel preset

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function(grunt) {
          */
         browserify: {
             options: {
-                transform: [['babelify', {presets: ['es2015']}]],
+                transform: [['babelify', {presets: ['env']}]],
             },
             lib: {
                 src: 'src/**/*.js',
