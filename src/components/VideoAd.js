785f3d5b7c0674436e03f70512b403381df1f10b
Gultekin Boyraz <g.boyraz@azerion.com>
2019-06-26 14:31:33 +0200

    Failure of initial ad request skipped by arthur

--- src/components/VideoAd.js
+++ src/components/VideoAd.js
@@ -532,13 +532,13 @@ class VideoAd {
         try {
             let vastUrl = await this._requestAd(adType);
 
-            // TODO: test.
-            // If we assume the first ad fails (preroll).
-            if (!initialAd && adType === 'interstitial') {
-                vastUrl = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=';
-            } else if (adType === 'rewarded') {
-                vastUrl = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinearvpaid2js&correlator=';
-            }
+            // // TODO: test.
+            // // If we assume the first ad fails (preroll).
+            // if (!initialAd && adType === 'interstitial') {
+            //     vastUrl = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=';
+            // } else if (adType === 'rewarded') {
+            //     vastUrl = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinearvpaid2js&correlator=';
+            // }
 
             const adsRequest = await this._loadAd(vastUrl);
 
