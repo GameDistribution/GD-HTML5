ac9741299a92e960a763bab9d7895dbec937eb32
Gultekin Boyraz <g.boyraz@azerion.com>
2019-11-03 16:27:35 +0100

    move splash section to seperate folder

--- splash/base.js
+++ splash/base.js
@@ -1,27 +1,10 @@
 import { EventEmitter } from "events";
 
 class Base extends EventEmitter {
-  constructor(config) {
+  constructor(options,gameData) {
     super();
-  }
-  _init() {
-    let root = document.createElement("div");
-    let style = {
-      position: "absolute",
-      top: "0",
-      left: "0",
-      width: "100%",
-      height: "100%",
-      "box-sizing": "border-box",
-      display: "flex",
-      "flex-direction": "column",
-      "background-color": "green"
-    };
-    Object.assign(root.style, style);
-    this._root = root;
-  }
-  getRoot() {
-    return this._root;
+    this.options=options;
+    this.gameData=gameData;
   }
 }
 
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,10 +1,12 @@
 import Quantum from "../splash/quantum";
+import gameData from "./gameData";
+import options from "./options";
 
 export default {
   title: "Splash"
 };
 
 export const quantum = () => {
-  const splash = new Quantum();
+  const splash = new Quantum(options, gameData.result.game);
   return splash.getRoot();
 };
