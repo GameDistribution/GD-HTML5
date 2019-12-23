2644a6242f1a648c2f4dc113b069eb7ee27db80d
Gultekin Boyraz <g.boyraz@azerion.com>
2019-12-23 19:51:08 +0300

    checkpoint

--- src/components/Macros.js
+++ src/components/Macros.js
@@ -10,7 +10,7 @@ class Macros {
         this.game = ctx.game;
         this.bridge = ctx.bridge;
 
-        // console.log(this);
+        console.log(this.game);
     }
 
     transform(value) {
@@ -22,19 +22,53 @@ class Macros {
         return cloned;
     }
 
-    transformValue(value, key) {
+    transformValue(value) {
         if (isPlainObject(value) || isArray(value)) { // ?
             for (let objKey in value) {
-                value[objKey] = this.transformValue(value[objKey], objKey);
+                value[objKey] = this.transformValue(value[objKey]);
             }
         } else if (isString(value)) {
             // replace params here
-            console.log(key, value);
-
-            
+            let regex = /\{\{(\w+)\}\}/g;
+            let matched;
+            let replaceItems = [];
+            do {
+                matched = regex.exec(value);
+                if (matched) {
+                    let macro = matched[0];
+                    let macroKey = matched[1];
+                    let macroValue = this.getMacroKeyValue(macroKey);
+                    if (macroValue) {
+                        replaceItems.push({ key: macroKey, value: macroValue })
+                        // replace it
+                        console.log(macroValue);
+                    }
+                }
+            } while (matched);
+            if (replaceItems.length > 0) {
+                replaceItems.forEach(item => {
+                    value = value.replace(new RegExp("\{\{" + item.key + "\}\}", "g"), item.value)
+                });
+            }
         }
-
         return value;
+    }
+    getMacroKeyValue(key) {
+        switch (key) {
+            case "PAGE_URL":
+                return this.bridge.parentURL;
+            case "PAGE_URL_ESC":
+                return encodeURIComponent(this.bridge.parentURL);
+            case "PAGE_URL_ESC_ESC":
+                return encodeURIComponent(encodeURIComponent(this.bridge.parentURL));
+            case "CACHEBUSTER":
+                return Date.now();
+            case "GAME_ID":
+                return this.game.gameId;
+        }
+    }
+    _escapeRegExp(string) {
+        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
     }
 }
 
